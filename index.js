98f30045e5bc7f9a87abe7c03d01177b4335a659
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-20 20:32:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function showRepositories(event, data) {
+  console.log(this.responseText)
+  let repoList = "<ul>"
+  for(var i=0;i < this.responseText.length; i++) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  repoList += "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.open("GET", 'https://api.github.com/users/petitlapin86/repos')
+  req.send()
+}
